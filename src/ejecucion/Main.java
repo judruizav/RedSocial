fd2b202498c0db2211bc0d991347003c5249d384
Julian <Julian@Julian-PC>
2016-04-24 18:02:16 -0500

    new commit

--- src/ejecucion/Main.java
+++ src/ejecucion/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("No lo hagas perra...");
+        System.out.println("Nosotros somos... Los menores");
     }
     
 }
